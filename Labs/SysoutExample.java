bd11531673e49cadfccea14e242671dab7d7a3f5
Sheldon Pasciak <sheldon@pasciak.com>
2024-01-15 09:32:00 -0700

    Completed SysoutExample

---
+++ Labs/SysoutExample.java
@@ -0,0 +1,11 @@
+public class SysoutExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("These words will be printed.");
+		System.out.println("And so will these words.");
+		System.out.println("And finally these words.");
+
+	}
+
+}

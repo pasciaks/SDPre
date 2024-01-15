2fe8a1f7d22a2617ff6f0c5807915a0e5e707081
Sheldon Pasciak <sheldon@pasciak.com>
2024-01-15 09:39:07 -0700

    MenuExample completed

---
+++ Labs/MenuExample.java
@@ -0,0 +1,12 @@
+public class MenuExample {
+
+	public static void main(String[] args) {
+	    System.out.println("Welcome to My Bank Application!");
+	    System.out.println("-------------------------");
+	    System.out.println("1) Check Balance");
+	    System.out.println("2) Withdraw");
+	    System.out.println("3) Deposit");  
+	    System.out.println("-------------------------");
+	}
+
+}

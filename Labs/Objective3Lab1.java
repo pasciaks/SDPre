b67a90b9aab1744631ac3675b9c1c5e9316aef08
Sheldon Pasciak <sheldon@pasciak.com>
2024-01-17 08:11:47 -0700

    Complete Objective 3 Lab 1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,29 @@
+public class Objective3Lab1 {
+    public static void main(String[] args) {
+        double num1, num2, num3, average;
+
+        /*
+            Using the starter code below, add code to the commented area that would:
+            Assign values to num1, num2, and num3.
+            Calculate the average of those numbers and store the resulting value into average.
+            Print out the calculated average.
+         */
+
+        num1 = 15.0;
+        num2 = 6.0;
+        num3 = 2.0;
+        average = (num1 + num2 + num3) / 3;
+        System.out.println(average);
+
+        /*
+
+        // Note: There may be precision differences between the example and this Java 21 execution:
+        System.out.println("7.66666666667");
+
+        // Note: Perhaps a better way to do this would be to use the printf method:
+        System.out.printf("%.2f\n", average);
+
+         */
+
+    }
+}
